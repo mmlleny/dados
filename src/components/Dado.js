362c1ebf7f157781b6c0e71185881365279adb2e
Marcela Milleny <165828516+mmlleny@users.noreply.github.com>
2025-03-18 01:52:53 +0000

    Pending changes exported from your codespace

--- src/components/Dado.js
+++ src/components/Dado.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-function Dado({ valor }) {
-    const imagens = {
-        1: '/img/dado1.png',
-        2: '/img/dado2.png',
-        3: '/img/dado3.png',
-        4: '/img/dado4.png',
-        5: '/img/dado5.png',
-        6: '/img/dado6.png',
-      };
+// componente que exibe a imagem do dado de acordo com o número sorteado
+function Dado({ numero }) {
+  const imagem = `/img/dado${numero}.png`;
+
   return (
-    <div>
-      <img src={imagens[valor]} alt={`Dado número ${valor}`} width="100" />
-    </div>
+    <img 
+      src={imagem} 
+      alt={`Dado com número ${numero}`} 
+      style={{ width: '100px', height: '100px' }} 
+    />
   );
 }
 
